960149875794f0231361d329e204c62e92b750a5
Hassan El Ouardy <hassan.elouardy06@gmail.com>
2025-12-19 20:38:10 +0100

    make auth as home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import HomePage from "@/src/components/HomePage";
+import AuthForm from "@/src/components/AuthPage";
 export default function Home() {
-	return <HomePage />;
+	return <AuthForm />;
 }
